8ca701ea007a62ba4c0189452d6e8c05f9466953
Jassar <jaz.abbasi@gmail.com>
2025-06-19 16:11:29 +0400

    telemetry pre-test (day 1)

---
+++ dashboard/telemetry.py
@@ -0,0 +1,25 @@
+import socket
+import threading
+
+class TelemetryReciever:
+    def __init__(self, port=5555):
+        self.host = "0.0.0.0"
+        self.port = port
+        self.running = False
+        self.data_callback = None
+
+    def start(self):
+        self.running = True
+        thread = threading.Thread(target=self._receive) # Creates a background thread to run the recieve method
+        thread.daemon = True
+        thread.start
+
+    def _recieve(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        while self.running:
+            data, _ = sock.recvfrom(1024) #Set buffer size ; The underscore is used to ignore the 2nd return of recvfrom()
+            if self.data_callback:
+                self.data_callback(data)
+    
+    def stop(self):
+        self.running = False
