35a6c3b653c4f98f3c42f1ddf7c9881feae0df98
Jassar <jaz.abbasi@gmail.com>
2025-06-22 19:19:35 +0400

    Added GUI elements

--- dashboard/gui.py
+++ dashboard/gui.py
@@ -16,30 +16,49 @@ canvas.pack(fill="both", expand=True)
 cx = screen_width // 2
 cy = screen_height // 2
 
+
 # Gear
 gear = Label(screen, font=("Impact", 250), text="N", fg="white", bg="#3f3f3f")
 canvas.create_window(cx, cy, window=gear)
-
 gear_label = Label(screen, font=("Impact", 60), text="Gear", bg="#3f3f3f", fg="white")
 canvas.create_window(cx, cy + 160, window=gear_label)
 
 # RPM
 rpm = Label(screen, font=("Impact", 100), text="00000", fg="white", bg="#3f3f3f")
 canvas.create_window(cx, cy - 300, window=rpm)
-
 rpm_label = Label(screen, font=("Impact", 30), text="RPM", fg="white", bg="#3f3f3f")
 canvas.create_window(cx, cy - 210, window=rpm_label)
 
 # Speed
 speed = Label(screen, font=("Impact", 100), text="000", fg="white", bg="#3f3f3f")
-canvas.create_window(cx, cy + 300, window=speed)
-
+canvas.create_window(cx, cy + 280, window=speed)
 speed_label = Label(screen, font=("Impact", 30), text="km/h", fg="white", bg="#3f3f3f")
-canvas.create_window(cx, cy + 390, window=speed_label)
+canvas.create_window(cx, cy + 370, window=speed_label)
 
-# Exit fullscreen with ESC
+# Lap Number
+lap = Label(screen, font=("Impact", 70), text="00", fg="black")
+canvas.create_window(cx-530, cy-330, window=lap)
+lap_label = Label(screen, font=("Impact", 30), text="Lap", fg="white", bg="#3f3f3f")
+canvas.create_window(cx-530, cy-230, window=lap_label)
+
+# Position
+pos = Label(screen, font=("Impact", 70), text="00", fg="black")
+canvas.create_window(cx-700, cy-330, window=pos)
+pos_label = Label(screen, font=("Impact", 30), text="POS", fg="white", bg="#3f3f3f")
+canvas.create_window(cx-700, cy-230, window=pos_label)
+
+# Current Lap
+current_lap_label = Label(screen, font=("Impact", 50), text="Current lap", fg="white", bg="#3f3f3f")
+canvas.create_window(cx-770, cy, window=current_lap_label)
+
 def exit_fullscreen(event):
     screen.attributes("-fullscreen", False)
+    print("workssss")
 
+def enter_fullscreen(event):
+    screen.attributes("-fullscreen", True)
 screen.bind("<Escape>", exit_fullscreen)
+screen.bind("<F11>", enter_fullscreen)
+
+
 screen.mainloop()
